888991f24699754a10825d817a9dd0aca03ec6e2
ericalvarado <ericalvarado@gmail.com>
2015-05-29 13:59:20 -0400

    Completed Lesson 58 - Multiple View Controllers.

---
+++ RSS/DetailedViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailedViewController.swift
+//  RSS
+//
+//  Created by Eric Alvarado on 5/29/15.
+//  Copyright (c) 2015 Eric Alvarado. All rights reserved.
+//
+
+import UIKit
+
+class DetailedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
